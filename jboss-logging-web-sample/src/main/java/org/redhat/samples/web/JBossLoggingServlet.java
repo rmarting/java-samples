437cba552cfaddec3985d2729fc4898e5b5aaebc
rmarting - Roman Martin Gil <rmarting@redhat.com>
2014-10-20 22:33:07 +0200

    Initial import

---
+++ jboss-logging-web-sample/src/main/java/org/redhat/samples/web/JBossLoggingServlet.java
@@ -0,0 +1,104 @@
+package org.redhat.samples.web;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.logging.Logger;
+
+/**
+ * Servlet implementation class JBossLoggingServlet
+ */
+public class JBossLoggingServlet extends HttpServlet {
+
+	/** JBoss Logger */
+	private static final long serialVersionUID = 2518824528761300162L;
+
+	private static final Logger LOGGER = Logger
+			.getLogger(JBossLoggingServlet.class);
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public JBossLoggingServlet() {
+		super();
+
+		LOGGER.info("JBoss Logging Servlet created");
+	}
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		LOGGER.info("JBoss Logging Servlet initiated successfully");
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	@Override
+	public void destroy() {
+		LOGGER.info("JBoss Logging Servlet destroyed");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void service(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// Level
+		String level = request.getParameter("level");
+		if (null == level) {
+			level = "INFO";
+		}
+		// Message
+		String message = request.getParameter("message");
+		if (null == message) {
+			message = "This is a default message";
+		}
+
+		// Evaluate level to log the message
+		switch (level) {
+		case "DEBUG":
+			LOGGER.debug(message);
+			break;
+
+		case "TRACE":
+			LOGGER.trace(message);
+			break;
+
+		case "WARN":
+			LOGGER.warn(message);
+			break;
+
+		case "ERROR":
+			LOGGER.error(message);
+			break;
+
+		case "FATAL":
+			LOGGER.fatal(message);
+			break;
+
+		case "EXCEPTION":
+			LOGGER.error(message, new RuntimeException(
+					"This is a new RuntimeException"));
+			break;
+
+		default:
+			LOGGER.info(message);
+		}
+
+		response.getWriter().write(
+				"Message logged with '" + level
+						+ "' level in JBoss EAP log file");
+	}
+
+}
---
+++ jboss-logging-web-sample/src/main/webapp/index.jsp
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>JBoss Logging Web Application</title>
+</head>
+<body>
+	<h2>JBoss Logging Web Application!</h2>
+	
+	<div>
+		<ul>
+			<li><a href=".//JBossLoggingServlet?level=DEBUG&message=this is a debug message">Debug Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=TRACE&message=this is a trace message">Trace Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=INFO&message=this is a info message">Info Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=WARN&message=this is a warning message">Warning Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=ERROR&message=this is a error message">Error Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=FATAL&message=this is a fatal message">Fatal Message</a></li>
+			<li><a href=".//JBossLoggingServlet?level=EXCEPTION&message=this is a exception message">Exception Message</a></li>	
+		</ul>
+	</div>
+</body>
+</html>
